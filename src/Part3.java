ef38c60b28b336dc1287115c61e65618acb075e8
Vasyl Onufriyev <VOnufriyev@thisisfusion.com>
2019-08-02 11:05:20 -0500

    fixed part 3 slightly

--- src/Part3.java
+++ src/Part3.java
@@ -1,14 +1,15 @@
 import java.security.MessageDigest;
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Part3 implements PartInterface {
 
-    private ReturnValue resval;
+    private List<ReturnValue> resValSet = new ArrayList<>();
 
     class ReturnValue {
 
-        ReturnValue()
-        {
+        ReturnValue() {
             weight = 21470000;
         }
 
@@ -63,8 +64,9 @@ public class Part3 implements PartInterface {
         return hashResultString;
     }
 
-    private ReturnValue GetLowestHashValue() {
+    private ReturnValue GetLowestHashValue(long timeInSecondsSet) {
         ReturnValue returnVal = new ReturnValue();
+        returnVal.setWeight(0);
         MessageDigest md = null;
 
         try {
@@ -74,28 +76,35 @@ public class Part3 implements PartInterface {
             System.exit(-1);
         }
 
-        String output;
+        ReturnValue returnValTemp = null;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(String.format("Starting iteration %d of %d of length %d seconds", i, 10, timeInSecondsSet));
+            String output;
+            returnValTemp = new ReturnValue();
+            long currentTime = System.nanoTime();
 
-        long currentTime = System.nanoTime();
+            while (System.nanoTime() < currentTime + (timeInSecondsSet * 1e9)) {
+                String input = GenerateStringRand();
+                returnValTemp.setInput(input);
+                output = AttemptHash(input, md);
 
-        while (System.nanoTime() < currentTime + 10e9) {
-            String input = GenerateStringRand();
-            returnVal.setInput(input);
-            output = AttemptHash(input, md);
-
-            if(returnVal.getWeight() > CalculateStringWeight(output)) {
-                returnVal.setOutput(output);
-                returnVal.setWeight(CalculateStringWeight(output));
+                if (returnValTemp.getWeight() > CalculateStringWeight(output)) {
+                    returnValTemp.setOutput(output);
+                    returnValTemp.setWeight(CalculateStringWeight(output));
+                }
             }
         }
 
+        returnVal.setWeight(returnVal.getWeight() + returnValTemp.getWeight());
+
+        returnVal.setWeight(returnVal.getWeight() / 10);
         return returnVal;
     }
 
     private int CalculateStringWeight(String text) {
         int total = 0;
 
-        for ( char item: text.toCharArray()) {
+        for (char item : text.toCharArray()) {
             total += item;
         }
 
@@ -118,11 +127,15 @@ public class Part3 implements PartInterface {
         allEnglishCharacters = new char['z' - 'a' + 1]; //https://stackoverflow.com/questions/8710719/generating-an-alphabetic-sequence-in-java YOOOINK
         for (char i = 'A'; i <= 'Z'; i++) allEnglishCharacters[i - 'A'] = i;
 
-        resval = GetLowestHashValue();
+        resValSet.add(GetLowestHashValue(10));
+        resValSet.add(GetLowestHashValue(20));
+        resValSet.add(GetLowestHashValue(30));
     }
 
     @Override
     public void PrintResult() {
-        System.out.println(String.format("Input String: %s-> Hash String: %s->\tWeight: %d", resval.getInput(), resval.getOutput(), resval.getWeight()));
+        for (ReturnValue resVal : resValSet) {
+            System.out.println(String.format("Input String: %s-> Hash String: %s->\tWeight: %d", resVal.getInput(), resVal.getOutput(), resVal.getWeight()));
+        }
     }
 }
